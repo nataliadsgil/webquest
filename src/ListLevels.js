9a24ddfc22b69a7107e216982b93b0ff36c11098
Natalia Silva <nataliadasilva.dev@gmail.com>
2020-02-11 19:00:04 -0300

    Construindo a tela de fases do jogo.

--- src/ListLevels.js
+++ src/ListLevels.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
 
+import {
+	Grid
+} from '@material-ui/core'
+
+import './ListLevels.css'
+
 class ListLevels extends Component {
 	constructor(props) {
 	  super(props);
@@ -9,7 +15,26 @@ class ListLevels extends Component {
 
 	render() {
 		return (
-			<h1>Levels List</h1>
+			<Grid container item direction="row" justify="center">
+		      <Grid container item direction="row" className="menu-bar" md={12}>
+		        <h1 style={{lineHeight: "5px", marginLeft: '20px', color: '#fff'}}>Traduja</h1>
+		      </Grid>
+		      	
+		      	<Grid container item direction="row" justify="center" md={10}>
+		      		<Grid container item md={2} justify="center" classes={{root: 'level-card'}}>Cores</Grid>
+		      		<Grid container item md={2} justify="center" classes={{root: 'level-card'}}>Frutas</Grid>
+		      		<Grid container item md={2} justify="center" classes={{root: 'level-card'}}>Animais</Grid>
+		      		<Grid container item md={2} justify="center" classes={{root: 'level-card'}}>Lugares</Grid>
+		      		<Grid container item md={2} justify="center" classes={{root: 'level-card'}}>Objetos</Grid>
+
+		      		<Grid container item md={2} justify="center" classes={{root: 'level-card'}}>Adjetivos</Grid>
+		      		<Grid container item md={2} justify="center" classes={{root: 'level-card'}}>Coletivos</Grid>
+		      		<Grid container item md={2} justify="center" classes={{root: 'level-card'}}>Superlativos</Grid>
+		      		<Grid container item md={2} justify="center" classes={{root: 'level-card'}}>Advérbios de Tempo</Grid>
+		      		<Grid container item md={2} justify="center" classes={{root: 'level-card'}}>Advérbios de Intensidade</Grid>
+		      	</Grid>
+
+		    </Grid>
 		)
 	}
 }
